319be874b21776f41abb8d567c805a9607280ea5
TeBeCo <tebeco@gmail.com>
2020-04-09 15:14:43 +0200

    gfjhfgj
    hjhfg

--- IOptionsFromLibs.Consumer/Program.cs
+++ IOptionsFromLibs.Consumer/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+jhnfdghjnfghxdbhsfdhusing Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
